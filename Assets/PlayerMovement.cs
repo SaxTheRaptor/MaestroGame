960112240e18695eb5345c56b804749cde7fe073
QuiTheVelociraptor <randomvelociraptorgaming@gmail.com>
2020-11-21 13:57:04 -0500

    Player movement
    Code for player movement and animations.

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    public Rigidbody2D rb;
+    public Animator anim;
+    private Vector2 moveDirection;
+    private Vector2 lastMoveDirection;
+    // Update is called once per frame
+    void Update()
+    {
+        //input
+
+        /* if (movementDirection != Vector2.0) { 
+         pass
+         }*/
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        if((moveX == 0 && moveY == 0) && moveDirection.x != 0 || moveDirection.y != 0)
+        {
+            lastMoveDirection = moveDirection;
+        }
+
+        moveDirection = new Vector2(moveX, moveY).normalized;
+        Animate();
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
+    }
+
+
+    void Animate()
+    {
+        anim.SetFloat("Horizontal", moveDirection.x);
+        anim.SetFloat("Vertical", moveDirection.y);
+        anim.SetFloat("Speed", moveDirection.sqrMagnitude);
+        anim.SetFloat("AnimLastMoveX", lastMoveDirection.x);
+        anim.SetFloat("AnimLastMoveY", lastMoveDirection.y);
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
+ 
+    
