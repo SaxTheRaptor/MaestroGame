959d807535208fe6174059053887845e7e93233f
Xurya <xur1@bxscience.edu>
2021-01-01 22:22:10 -0500

    Character Class and Overworld Bugfix?

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    public string name = "Placeholder";
+    public int health = 100;
+    public List<Skill> skills; //TOPD: Implement
+    
+    public void TakeDamage(int damage){
+        if(health < damage){
+            health = 0;
+            //TODO: set vars for anims
+            Death();
+            return;
+        }
+        health -= damage;
+    }
+
+    public void Death(){
+        Debug.Log(name + ": Argh..."); //placeholder. Can use dialogue.
+    }
+}
---
+++ Assets/Scripts/Trap.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trap : MonoBehaviour
+{
+    public int damage = 50;
+
+    public void Action(Character character){
+        character.TakeDamage(damage);
+    }
+
+    public void OnTriggerEnter2D(Collider2D other){
+        Character character = other.gameObject.GetComponent<Character>();
+        Debug.Log(character.name + " stepped on a trap!");
+        Action(character);
+    }
+}
