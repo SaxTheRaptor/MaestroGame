44885ceed15d06a5d81f212c224dcb8843fcac17
Ryan Xu <ryanxu@buffalo.edu>
2021-07-23 21:55:16 -0400

    Dialogue Bugfix

--- Assets/Scripts/Interactable.cs
+++ Assets/Scripts/Interactable.cs
@@ -12,7 +12,7 @@ public class Interactable : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(interactKey)){
+        if(isInRange && Input.GetKeyDown(interactKey)){
             interactAction.Invoke();
         }
     }
