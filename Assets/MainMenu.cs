4053a860cb74f7364de0ce6fd11f0506e31cc653
DGM <80726162+DIn0ziK@users.noreply.github.com>
2022-01-08 17:51:16 -0800

    First Scene(beta) + Title Screen
    if this doesnt work then so help me

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void LoadGame()
+    {
+        SceneManager.LoadScene("OverWorld");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("KT Exinction Event");
+        Application.Quit();
+    }
+
+    public void NewGame()
+    {
+        Debug.Log("https://store.steampowered.com/app/837470/Untitled_Goose_Game/");
+    }
+}
