d0f33e22c2b7f7cc568c4f2dd83eae5fa1ac2581
Lucas Baneke <42550065+Shaw358@users.noreply.github.com>
2022-01-30 21:36:46 +0100

    Item Class Update

--- Assets/Scripts/Shops/item.cs
+++ Assets/Scripts/Shops/item.cs
@@ -1,21 +1,77 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class item : MonoBehaviour
+﻿public enum ItemType
 {
-    // Start is called before the first frame update
-    public int cost;
-    public string itemname;
+    Artifacts,
+    Key,
+    Consumable,
+    Material,
+    Weapon
+}
 
-    public void bought()
+public class Item
+{
+    protected int cost;
+    protected string itemName;
+    private string description;
+    protected bool stackable;
+    private int stack;
+    private ItemType itemType;
+
+    //---=Init=---\\
+
+    public Item(int newCost, string newItemName, string newDescription, bool newStackable, ItemType newItemType)
     {
-        if(GetComponentInParent<shop>().money >= cost)
+        cost = newCost;
+        itemName = newItemName;
+        description = newDescription;
+        stackable = newStackable;
+        itemType = newItemType;
+    }
+
+    //---=Item Utilization=---\\
+
+    protected virtual bool CanUseItem(object[] args = null)
+    {
+        //NOTE: Optional Implementation
+        return true;
+    }
+
+    public virtual void UseItem(params object[] args)
+    {
+        //NOTE: Each element of args should be casted into their own class or data types
+        //NOTE: The best thing to do would be to create a standard for each element in the array with something like an enum
+        //Example: PlayerPrefs prefs = (PlayerPrefs)args[ParamType.PlayerPrefs];
+        //or the int route: PlayerPrefs prefs = (PlayerPrefs)args[0];
+
+        if (CanUseItem())
         {
-            GetComponentInParent<shop>().money -= cost;
-            GetComponentInParent<shop>().additem(itemname);
-
+            stack--;
         }
+    }
 
+    public void IncreaseItemStack(int amount)
+    {
+        stack = (stack + amount) < InventoryConsts.maxItemStack ? stack += amount : stack;
+    }
+
+    //---=Getters=---\\
+
+    public int GetCost()
+    {
+        return cost;
+    }
+
+    public int GetItemStackCount()
+    {
+        return stack;
+    }
+
+    public string GetDescription()
+    {
+        return description;
+    }
+
+    public ItemType GetItemType()
+    {
+        return itemType;
     }
 }
