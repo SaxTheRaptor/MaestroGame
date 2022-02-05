04504ebd772075926a961d3437fad88cfb502410
DGM <80726162+DIn0ziK@users.noreply.github.com>
2022-02-05 13:43:34 -0800

    Wallet Class + somethin
    The wallet beta, we should get some anims and UI for it. Theres also an item i tried to load into the world but it wont work but its there and its meant to be the test for the class.

---
+++ Assets/Scripts/Wallet.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wallet : MonoBehaviour
+{
+    public int Moneys = 0;
+
+    //gives ya money
+    public void addMoneys(int quantity)
+    {
+        if ((quantity + Moneys) > 1000) {
+            Debug.Log("You have enough money!")
+        } else { 
+            Debug.Log(quantity + Moneys);
+            Moneys += quantity;
+        }
+        //TODO: some sort of UI element + anim
+        
+    }
+
+    //takes away ur money
+    public void subMoneys(int quantity)
+    {
+        if ((quantity - Moneys) < 0)
+        {
+            Debug.Log("https://www.poorpeoplescampaign.org/")
+        } else { 
+            Debug.Log(quantity - Moneys);
+            Moneys -= quantity;
+        }
+        //TODO: some sort of UI element + anim
+    }
+
+    public int getMoneys()
+    {
+        return Moneys;
+        Debug.Log( Moneys );
+    }
+
+}
