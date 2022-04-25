b846c5d032e71f7304e162afc14e9db769a131cf
QuiTheVelociraptor <randomvelociraptorgaming@gmail.com>
2022-04-24 20:03:19 -0400

    Game Dev is Pain

---
+++ Assets/Scripts/Overworld/FadeInObject.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeInObject : MonoBehaviour
+{
+    // Start is called before the first frame update
+    SpriteRenderer rend;
+    void Start()
+    {
+        rend = GetComponent<SpriteRenderer>();
+        Color c = rend.material.color;
+        c.a = 0f;
+        rend.material.color = c;
+    }
+
+  
+
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+    
+            IEnumerator FadeIn()
+            {
+                for (float f = 0.5f; f <= 1; f += 0.05f)
+                {
+                    Color c = rend.material.color;
+                    c.a = f;
+                    rend.material.color = c;
+                    yield return new WaitForSeconds(0.05f);
+                }
+            }
+
+
+            void startFading()
+            {
+                StartCoroutine("FadeIn");
+            }
+        
+    }
+}
---
+++ Assets/Scripts/Overworld/FadeOut.cs
@@ -0,0 +1,20 @@
+﻿/*using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeOut : MonoBehaviour
+{
+    public void OnTriggerEnter2D(Collider2D other)
+    { 
+        private float speed = 0.01f;
+         Color color = renderer.material.color;
+         color.a -= speed;
+         renderer.material.color = color;
+        if (other.tag == "Player")
+        {
+           null; 
+        }
+
+    }
+
+}*/
